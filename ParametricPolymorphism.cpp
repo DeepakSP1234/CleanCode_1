3bc47524748720a0f21bdad54003f7d70dc00d3e
DeepakSP1234 <deepaksp.shimoga@gmail.com>
2024-11-18 10:47:24 +0530

    Using template

--- ParametricPolymorphism.cpp
+++ ParametricPolymorphism.cpp
@@ -1,24 +1,8 @@
-    
-    vector<string> Filter(vector<string> data, function <bool (const string&)> func)
+    template <typename T>
+    vector<T> Filter(vector<T> data, function <bool (const T&)> func)
     {
         
-      vector<string> retList;
-      // iterate  
-      for (auto name: data)
-        {
-            // invoke predicate
-            if (func (name))
-               // aggregate result 
-               retList.push_back(name);
-        }
-        // project the result
-        return retList;        
-    }
-
-    vector<int> Filter(vector<int> data, function <bool (const int&)> func)
-    {
-        
-      vector<int> retList;
+      vector<T> retList;
       // iterate  
       for (auto name: data)
         {
