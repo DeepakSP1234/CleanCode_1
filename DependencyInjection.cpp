ec53bc3e7dc79cda21a88453b4318452c22345a4
DeepakSP1234 <deepaksp.shimoga@gmail.com>
2024-11-21 10:33:26 +0530

    Create DependencyInjection.cpp

---
+++ DependencyInjection.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class BoschTPMS{
+      public:
+     ~BoschTPMS(){
+            cout<<"BoschTPMS Destructed"<<endl;
+        }
+
+      BoschTPMS(){
+            cout<<"BoschTPMS Constructed"<<endl;
+        }
+};
+
+class Engine{
+    public:
+     ~Engine(){
+            cout<<"Engine Destructed"<<endl;
+        }
+        
+      Engine(){
+            cout<<"Engine Constructed"<<endl;
+        }
+};
+
+class Car{
+    Engine engine;
+    BoschTPMS *tpms_ptr;
+
+    public:
+        Car(BoschTPMS& tpms){
+            tpms_ptr = &tpms;
+            cout<<"Car Constructed"<<endl;
+        }
+
+        ~Car(){
+            cout<<"Car Destructed"<<endl;
+        }
+};
+
+BoschTPMS tpms_frontright;
+
+void buildCar(){
+    Car alto(tpms_frontright);
+}
+
+int main() {
+    // Write C++ code here
+    buildCar();
+    cout<< "end of main" << endl;
+    return 0;
+}
+ 
